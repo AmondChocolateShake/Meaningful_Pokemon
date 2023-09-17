27eb8aa2292597c8bd573c16e71e8d0226bedb2b
AmondChocolateShake <dongju230130@gmail.com>
2023-09-17 23:41:33 +0900

    Pokemon 모델 작업중

---
+++ client/src/model/PokeModel.js
@@ -0,0 +1,52 @@
+
+
+export default class PokeModel{
+    pokemons=[{}];
+    fav_pokemons=[{}]
+    my_pokemons=[{}]
+
+    constructor(poke,fav,my){
+        this.pokemons=poke;
+        this.fav_pokemons=fav;
+        this.my_pokemons=my;
+    };
+
+    // fetchGetPoke(endpoint){
+    //     const path=endpoint;
+        
+    //     fetch(path)
+    //     .then(res=>res.json())
+    //     .then(data=>{
+    //         this.pokemons=data.pokemons;
+    //         this.fav_pokemons=data.fav_pokemons;
+    //         this.my_pokemons=data.my_pokemons;
+    //     })
+    // }
+    
+
+
+
+    //모든 포켓몬 리스트 반환
+    getPokemons(){
+        return this.pokemons;
+    }
+
+    //내가 좋아요 표시한 포켓몬 리스트 반환
+    getFavoritePokemons(){
+        return this.fav_pokemons;
+    }
+
+    //내가 수집한 포켓몬 리스트 반환
+    getMyPokemons(){
+        return this.my_pokemons;
+    }
+
+    //내가 수집한 포켓몬 삽입
+    pushNewCollection(pokemon){
+        this.my_pokemons.push(pokemon);
+    }
+
+
+
+
+}
