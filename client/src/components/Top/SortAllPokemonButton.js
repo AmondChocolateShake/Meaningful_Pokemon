a4ec19c74c5d5214c5f56dcdc13b8f97d8924124
KimEunA97 <kimeuna9712@gmail.com>
2023-09-17 23:45:55 +0900

    내비게이션 바 스타일링 중

---
+++ client/src/components/Top/SortAllPokemonButton.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './SortAllPokemonButton.css'
+
+export default function SortAllPokemonButton() {
+  
+  return (
+    <div className="AllPokemonButtonCss">
+      <div className="AllPokemonButtonTextCss">
+        All
+      </div>
+    </div>
+  )
+
+}
