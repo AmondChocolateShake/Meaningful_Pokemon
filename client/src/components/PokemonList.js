e13af5d548de72bd78f74ea918392686da9699d4
KimEunA97 <kimeuna9712@gmail.com>
2023-09-18 02:36:01 +0900

    데이터 어떻게 가공하지

--- client/src/components/PokemonList.js
+++ client/src/components/PokemonList.js
@@ -1,8 +1,12 @@
 import React from "react";
 import PokemonElement from "./PokemonElement";
-import './PokemonList.css';
+import './PokemonList.css'
 import SwitchingHeart from "./SwitchingHeart";
-import data from '../dummy.json';
+import data from '../dummy.json'
+
+const _pokemonData = JSON.parse(JSON.stringify(data));
+const _fav_pokemons = _pokemonData.fav_pokemons;
+console.log(_fav_pokemons);
 
 const pokemonData = {
   "1": "Bulbasaur",
@@ -27,9 +31,18 @@ function PokemonList() {
       {Object.entries(pokemonData).map(([id, name]) => (
         <div>
           <PokemonElement key={id} id={id} name={name} imgLink={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} />
-          <SwitchingHeart className="HeartPositionCss"/>
+          <SwitchingHeart className="HeartPositionCss" />
         </div>
       ))}
+      {/* <div>
+        {Object.entries(_fav_pokemons).map(([id, name,imgLink]) => (
+          <PokemonElement key={id} id={id} name={name} imgLink={imgLink} />
+        ))}
+      </div> */}
+
+      <div>
+        <PokemonElement key={_fav_pokemons[0].id} id={_fav_pokemons[0].id} name={_fav_pokemons[0].name} imgLink={_fav_pokemons[0].imgLink} />
+      </div>
     </div>
   )
 }
