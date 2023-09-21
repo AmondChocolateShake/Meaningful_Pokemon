6397f192fe844bad7bfb6c8700c198236fc8c55f
KimEunA97 <kimeuna9712@gmail.com>
2023-09-21 23:24:27 +0900

    컨트롤러 주석 업데이트

--- client/src/controller/PokemonController.js
+++ client/src/controller/PokemonController.js
@@ -1,12 +1,13 @@
+//모든 포켓몬 가져오기.
 function getAllPokemons(req,res) {
-  const data = getPokemonData();
+  const data = getPokemons();
   res.json(data);
 }
 
+//포켓몬 데이터 업데이트
 function updatePokemonData(updatedData) {
   pokemonData = updatedData;
 }
-
 function updatePokemon(req, res) {
   const updatedData = req.body;
   updatePokemonData(updatedData);
