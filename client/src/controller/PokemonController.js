11f7c93bd9300a05ff141c81eaa25c2bc5ca7e46
KimEunA97 <kimeuna9712@gmail.com>
2023-09-21 23:13:59 +0900

    데이터 관련 함수는 어떻게 짜야

--- client/src/controller/PokemonController.js
+++ client/src/controller/PokemonController.js
@@ -7,12 +7,6 @@ function updatePokemonData(updatedData) {
   pokemonData = updatedData;
 }
 
-function getAllPokemon(req, res) {
-  const data = getPokemonData();
-  res.json(data);
-}
-
-// 포켓몬을 업데이트하는 컨트롤러 액션
 function updatePokemon(req, res) {
   const updatedData = req.body;
   updatePokemonData(updatedData);
