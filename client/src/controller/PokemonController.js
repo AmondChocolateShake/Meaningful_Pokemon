ce1a29aaa944ca591e7db697a8412634efcb3f03
KimEunA97 <kimeuna9712@gmail.com>
2023-09-21 23:10:49 +0900

    업데이트 함수???

--- client/src/controller/PokemonController.js
+++ client/src/controller/PokemonController.js
@@ -2,3 +2,19 @@ function getAllPokemons(req,res) {
   const data = getPokemonData();
   res.json(data);
 }
+
+function updatePokemonData(updatedData) {
+  pokemonData = updatedData;
+}
+
+function getAllPokemon(req, res) {
+  const data = getPokemonData();
+  res.json(data);
+}
+
+// 포켓몬을 업데이트하는 컨트롤러 액션
+function updatePokemon(req, res) {
+  const updatedData = req.body;
+  updatePokemonData(updatedData);
+  res.sendStatus(200);
+}
