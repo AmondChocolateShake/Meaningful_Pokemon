fce2e5e0399aa744a316673ecdd84c58dcf473b6
KimEunA97 <kimeuna9712@gmail.com>
2023-09-22 20:20:34 +0900

    모델 함수 import해서 데이터 오가는 로직 구현

--- client/src/controller/PokemonController.js
+++ client/src/controller/PokemonController.js
@@ -1,15 +1,25 @@
+import PokeModel from "../model/PokeModel";
+
+const pokeModel = new PokeModel([], [], []);
+
+
 //모든 포켓몬 가져오기.
-function getAllPokemons(req,res) {
-  const data = getPokemons();
+function getAllPokemons(req, res) {
+  const data = pokeModel.getPokemons();
   res.json(data);
 }
 
 //포켓몬 데이터 업데이트
-function updatePokemonData(updatedData) {
-  pokemonData = updatedData;
-}
+// function updatePokemonData(updatedData) {
+//   pokemonData = updatedData;
+// }
 function updatePokemon(req, res) {
   const updatedData = req.body;
-  updatePokemonData(updatedData);
+  pokeModel.updatePokemonData(updatedData);
   res.sendStatus(200);
+}
+
+function getFavoritePokemons(req, res) {
+  const data = pokeModel.getFavoritePokemons();
+  res.json(data);
 }
