e62a5d9fbb8abdf21266674ddc7a0143fbd2b8b8
KimEunA97 <kimeuna9712@gmail.com>
2023-10-14 02:23:19 +0900

    하트 이미지 파일 삽입

--- client/src/components/BlackHeart.js
+++ client/src/components/BlackHeart.js
@@ -1,10 +1,11 @@
 import React from "react";
+import blackHeart from '../img/blackHeart.png'
 
 export default function BlackHeart(){
 
   return (
     <div>
-          <img alt="Not Like it." src="https://github.com/AmondChocolateShake/Meaningful_Pokemon/assets/124133601/e89f4c29-9505-499e-bed2-3b21ed1c68d9"  style={{ width: "25px", height: "25px"}} ></img>
+          <img alt="Not Like it." src={blackHeart}  style={{ width: "25px", height: "25px"}} ></img>
     </div>
   )
 
--- client/src/components/RedHeart.js
+++ client/src/components/RedHeart.js
@@ -1,9 +1,10 @@
 import React from "react";
+import redHeart from '../img/redHeart.png'
 
 export default function RedHeart(){
   return (
     <div>
-          <img alt="Like it!" src="https://github.com/AmondChocolateShake/Meaningful_Pokemon/assets/124133601/14fa8941-f4c0-4bfc-a752-f6276fc3169d" style={{ width: "25px", height: "25px"}}></img>
+          <img alt="Like it!" src={redHeart} style={{ width: "25px", height: "25px"}}></img>
 
     </div>
   )
