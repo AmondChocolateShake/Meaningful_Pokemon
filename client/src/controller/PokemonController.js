39faa904c92d5f98d3a0ba76a1b6eb80057930f8
KimEunA97 <kimeuna9712@gmail.com>
2023-10-16 11:44:40 +0900

    컨트롤러 함수 export

--- client/src/controller/PokemonController.js
+++ client/src/controller/PokemonController.js
@@ -3,31 +3,24 @@ import PokeModel from '../model/PokeModel.js';
 const pokeModel = new PokeModel([], [], []);
 
 //모든 포켓몬 가져오기.
-function getAllPokemons(req, res) {
+export function getAllPokemons(req, res) {
   const data = pokeModel.getPokemons();
   console.log(data)
   res.json(data);
 }
 
-
-
 //포켓몬 데이터 업데이트
-function updatePokemon(req, res) {
+export function updatePokemon(req, res) {
   const updatedData = req.body;
   pokeModel.pushNewCollection(updatedData);
   console.log(updatedData)
   res.sendStatus(200);
 }
 
-
-function getFavoritePokemons(req, res) {
+//좋아요 한 포켓몬 가져오기
+export function getFavoritePokemons(req, res) {
   const data = pokeModel.getFavoritePokemons();
   console.log(data)
   res.json(data);
 }
 
-const addPokemonData = [
-  { id: 4, name: '냐옹', type: '노말', owned: false, favorite: true },
-]
-
-// updatePokemon(pokemonData);
