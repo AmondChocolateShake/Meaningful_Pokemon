0dc725ae27a6e5e1b5c87897ada658020653a062
KimEunA97 <kimeuna9712@gmail.com>
2023-10-16 19:11:58 +0900

    db 기본 셋업

---
+++ db/src/db.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql'); // 또는 require('mysql2');
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+
+connection.connect((err) => {
+  if (err) {
+    console.error('MySQL 연결 오류:', err);
+    return;
+  }
+  console.log('MySQL에 연결되었습니다.');
+
+  // 이곳에서 SQL 쿼리 실행
+
+  connection.end(); // 연결 종료
+});
